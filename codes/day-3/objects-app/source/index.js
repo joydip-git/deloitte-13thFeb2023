3930e6f7cc80547d8fba82ba2b08b299329fd743
Joydip <joydip.mondal@gmail.com>
2023-02-15 12:39:46 +0530

    day-3 pre-lunch update

--- codes/day-3/objects-app/source/index.js
+++ codes/day-3/objects-app/source/index.js
@@ -1,53 +1,10 @@
-function local() {
-    let a = 100
-    this.x = 200
-}
-//constuctor function: used to store values with property names in an object
-function person(pname, pid, psalary, pdept, pprojects) {
-    this.name = pname
-    this.id = pid
-    this.department = pdept
-    this.salary = psalary
-    this.projects = pprojects
-    this.print = function () {
-        //templated string (string interpolation)
-        return `${this.name}, ${this.department}, ${this.salary}`
-        //return this.name + ' ' + this.department + ' ' + this.salary
-    }
-    //return this
-}
-const khushiObject = new person('khushi', 1001, 100000, 'EDU', ['CITA', 'STA'])
-const srirupObject = new person('srirup', 1002, 110000, 'LEARNING', ['A'])
-console.log(window)
-
-//accessing value properties from an object
-console.log(khushiObject.department)
-console.log(khushiObject['salary'])
-
-//accessing functional properties from an object
-const infoOfKhsuhi = khushiObject.print()
-const infoOfSrirup = srirupObject.print()
-//const info = khushiObject['print']()
-console.log(infoOfKhsuhi)
-console.log(infoOfSrirup)
-
-// console.log(khushiObject.print())
-
-function employee(pname, pid, psalary, pdept, pprojects) {
-    //object-literal syntax
-    let obj = {
-        name: pname,
-        id: pid,
-        department: pdept,
-        salary: psalary,
-        projects: pprojects,
-        print: function () {
-            //templated string (string interpolation)
-            return `${this.name}, ${this.department}, ${this.salary}`
-            //return this.name + ' ' + this.department + ' ' + this.salary
-        }
-    }
-    return obj
-}
-
-const amitObj = person('amit', 1003, 120000, 'DEP', ['B', 'C'])
+//constructor function to create product
+//every product object should have the following value properties
+/**
+ * id: unique id value of a product (number),
+ * name: name of the product (string), 
+ * description: description of the product (string), 
+ * price: price of the product (number), 
+ * rating:rating of the product (number, max 5), 
+ * imagePath: path of the image file of the product, as present in your system
+ */
---
+++ codes/day-3/objects-app/source/objects.js
@@ -0,0 +1,53 @@
+function local() {
+    let a = 100
+    this.x = 200
+}
+//constuctor function: used to store values with property names in an object
+function person(pname, pid, psalary, pdept, pprojects) {
+    this.name = pname
+    this.id = pid
+    this.department = pdept
+    this.salary = psalary
+    this.projects = pprojects
+    this.print = function () {
+        //templated string (string interpolation)
+        return `${this.name}, ${this.department}, ${this.salary}`
+        //return this.name + ' ' + this.department + ' ' + this.salary
+    }
+    //return this
+}
+const khushiObject = new person('khushi', 1001, 100000, 'EDU', ['CITA', 'STA'])
+const srirupObject = new person('srirup', 1002, 110000, 'LEARNING', ['A'])
+console.log(window)
+
+//accessing value properties from an object
+console.log(khushiObject.department)
+console.log(khushiObject['salary'])
+
+//accessing functional properties from an object
+const infoOfKhsuhi = khushiObject.print()
+const infoOfSrirup = srirupObject.print()
+//const info = khushiObject['print']()
+console.log(infoOfKhsuhi)
+console.log(infoOfSrirup)
+
+// console.log(khushiObject.print())
+
+function employee(pname, pid, psalary, pdept, pprojects) {
+    //object-literal syntax
+    let obj = {
+        name: pname,
+        id: pid,
+        department: pdept,
+        salary: psalary,
+        projects: pprojects,
+        print: function () {
+            //templated string (string interpolation)
+            return `${this.name}, ${this.department}, ${this.salary}`
+            //return this.name + ' ' + this.department + ' ' + this.salary
+        }
+    }
+    return obj
+}
+
+const amitObj = person('amit', 1003, 120000, 'DEP', ['B', 'C'])
